3d81bd1e5c4f38aae44e3a9cbb18b7c1a3742528
R. Campbell <robertcampbellnz@gmail.com>
2016-07-15 09:45:25 +1200

    (Add multiplayer lobby files, mistake to be untracked)

---
+++ Devious Retention Menu/MultiplayerLobby.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Devious_Retention_Menu
+{
+    partial class MultiplayerLobby
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitButton = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.factionBox = new System.Windows.Forms.TextBox();
+            this.colorBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.nameChooserLabel = new System.Windows.Forms.Label();
+            this.factionChooserLabel = new System.Windows.Forms.Label();
+            this.colorChooserLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // exitButton
+            // 
+            this.exitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exitButton.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(772, 549);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(200, 50);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nameBox.Location = new System.Drawing.Point(12, 579);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(168, 20);
+            this.nameBox.TabIndex = 2;
+            this.nameBox.TextChanged += new System.EventHandler(this.nameBox_TextChanged);
+            // 
+            // factionBox
+            // 
+            this.factionBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.factionBox.Location = new System.Drawing.Point(186, 579);
+            this.factionBox.Name = "factionBox";
+            this.factionBox.Size = new System.Drawing.Size(168, 20);
+            this.factionBox.TabIndex = 2;
+            this.factionBox.TextChanged += new System.EventHandler(this.factionBox_TextChanged);
+            // 
+            // colorBox
+            // 
+            this.colorBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.colorBox.Location = new System.Drawing.Point(360, 579);
+            this.colorBox.Name = "colorBox";
+            this.colorBox.Size = new System.Drawing.Size(168, 20);
+            this.colorBox.TabIndex = 2;
+            this.colorBox.TextChanged += new System.EventHandler(this.colorBox_TextChanged);
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(566, 549);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(200, 50);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // nameChooserLabel
+            // 
+            this.nameChooserLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nameChooserLabel.AutoSize = true;
+            this.nameChooserLabel.Location = new System.Drawing.Point(9, 560);
+            this.nameChooserLabel.Name = "nameChooserLabel";
+            this.nameChooserLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameChooserLabel.TabIndex = 3;
+            this.nameChooserLabel.Text = "Name";
+            // 
+            // factionChooserLabel
+            // 
+            this.factionChooserLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.factionChooserLabel.AutoSize = true;
+            this.factionChooserLabel.Location = new System.Drawing.Point(183, 560);
+            this.factionChooserLabel.Name = "factionChooserLabel";
+            this.factionChooserLabel.Size = new System.Drawing.Size(73, 13);
+            this.factionChooserLabel.TabIndex = 3;
+            this.factionChooserLabel.Text = "Faction Name";
+            // 
+            // colorChooserLabel
+            // 
+            this.colorChooserLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.colorChooserLabel.AutoSize = true;
+            this.colorChooserLabel.Location = new System.Drawing.Point(357, 560);
+            this.colorChooserLabel.Name = "colorChooserLabel";
+            this.colorChooserLabel.Size = new System.Drawing.Size(88, 13);
+            this.colorChooserLabel.TabIndex = 3;
+            this.colorChooserLabel.Text = "Color (6 digit hex)";
+            // 
+            // MultiplayerLobby
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 611);
+            this.Controls.Add(this.colorChooserLabel);
+            this.Controls.Add(this.factionChooserLabel);
+            this.Controls.Add(this.nameChooserLabel);
+            this.Controls.Add(this.colorBox);
+            this.Controls.Add(this.factionBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "MultiplayerLobby";
+            this.Text = "Lobby";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox factionBox;
+        private System.Windows.Forms.TextBox colorBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label nameChooserLabel;
+        private System.Windows.Forms.Label factionChooserLabel;
+        private System.Windows.Forms.Label colorChooserLabel;
+    }
+}
---
+++ Devious Retention Menu/MultiplayerLobby.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Devious_Retention_Menu
+{
+    /// <summary>
+    /// A game lobby has a bunch of controls which can be manipulated to create
+    /// settings for the game which will be launched from it.
+    /// </summary>
+    public partial class MultiplayerLobby : Form
+    {
+        #region Relative Size Constants
+        private const double TITLE_HEIGHT = 0.12;
+        private const double TITLE_FONT_SIZE = 0.06;
+        private const double FONT_SIZE = 0.03;
+        private const double MARGIN = 0.02;
+        private const double PLAYER_AREA_TOP = 0.2;
+        private const double TOTAL_PLAYER_HEIGHT = 0.7;
+
+        // Relative to the width of a player (nearly the width of the window)
+        private const double PLAYER_NUMBER_WIDTH = 0.1;
+        private const double PLAYER_NAME_WIDTH = 0.4;
+        #endregion
+        private const string FONT_NAME = "Arial";
+        private const int MAX_NAME_CHARS = 20;
+
+        private IPlayerChangeListener listener;
+
+        private ICollection<LobbyHost.ClientData> players;
+        private int playerID;
+
+        private Font titleFont;
+        private Font font;
+
+        private Label title;
+
+        private bool isHosting; // Whether or not this player is the host of the lobby
+        private LobbyHost host; // Only used if isHosting
+
+        private int playerHeight; // absolute
+        private int numPlayers;
+        private int margin;
+
+        /// <summary>
+        /// Creates a client lobby
+        /// </summary>
+        public MultiplayerLobby(IPlayerChangeListener listener)
+        {
+            this.listener = listener;
+            isHosting = false;
+            InitAll();
+        }
+
+        /// <summary>
+        /// Creates a host lobby
+        /// </summary>
+        public MultiplayerLobby(LobbyHost host, IPlayerChangeListener listener)
+        {
+            this.listener = listener;
+            isHosting = true;
+            this.host = host;
+            InitAll();
+        }
+
+        private void InitAll()
+        {
+            InitializeComponent();
+            ManualInit();
+            Paint += Render;
+        }
+
+        /// <summary>
+        /// Initialises some components of the window that aren't there on the
+        /// forms designer.
+        /// </summary>
+        private void ManualInit()
+        {
+            // Make title
+            title = new Label();
+            title.Text = "Multiplayer Lobby" + (isHosting ? " Host" : "");
+            title.TextAlign = ContentAlignment.MiddleCenter;
+            this.Controls.Add(title);
+        }
+
+        /// <summary>
+        /// Sets this lobby renderer's knowledge of the lobby players
+        /// to the given players.
+        /// </summary>
+        public void SetPlayers(ICollection<LobbyHost.ClientData> players, int playerID)
+        {
+            this.players = players;
+            numPlayers = players.Count;
+            this.playerID = playerID;
+        }
+
+        public void Render(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            if (!isHosting) startButton.Visible = false;
+            AdaptSizes();
+            DrawPlayers(g);
+        }
+
+        /// <summary>
+        /// Sets the sizes andd positions of all appropriate elements,
+        /// as well as font sizes, given the current size of the window
+        /// and amount of connected players.
+        /// </summary>
+        private void AdaptSizes()
+        {
+            font = new Font(FONT_NAME, (int)(FONT_SIZE * Math.Min(Width, Height)));
+            titleFont = new Font(FONT_NAME, (int)(TITLE_FONT_SIZE * Math.Min(Width, Height)));
+            margin = (int)(MARGIN * Math.Min(Width, Height));
+            
+            title.SetBounds(0, margin, Width, (int)(TITLE_HEIGHT * Height));
+            title.Font = titleFont;
+
+            double playerHeightWithMargin = Height * (TOTAL_PLAYER_HEIGHT / numPlayers);
+            playerHeight = (int)(playerHeightWithMargin - Height*MARGIN);
+        }
+
+        /// <summary>
+        /// Draws the player information to the best knowledge of
+        /// the lobby renderer on the given graphics.
+        /// </summary>
+        private void DrawPlayers(Graphics g)
+        {
+            int topY = (int)(PLAYER_AREA_TOP * Height);
+
+            foreach(LobbyHost.ClientData player in players)
+            {
+                int currentY = topY + (playerHeight + margin) * (player.playerNumber-1);
+                Point currentPoint = new Point(margin, currentY);
+
+                Color playerColor = player.color;
+                Brush playerBrush = new SolidBrush(playerColor);
+
+                g.DrawString(player.playerNumber+"", font, playerBrush, currentPoint);
+                currentPoint.X += (int)(Width * PLAYER_NUMBER_WIDTH);
+                g.DrawString(player.username, font, playerBrush, currentPoint);
+                currentPoint.X += (int)(Width * PLAYER_NAME_WIDTH);
+                g.DrawString(player.factionName + "", font, playerBrush, currentPoint);
+            }
+        }
+
+        /// <summary>
+        /// Informs the listener of a change to the client's name.
+        /// </summary>
+        private void nameBox_TextChanged(object sender, EventArgs e)
+        {
+            if (nameBox.Text.Length > MAX_NAME_CHARS) listener.UpdateClientUsername(nameBox.Text.Substring(0, MAX_NAME_CHARS));
+            else listener.UpdateClientUsername(nameBox.Text);
+        }
+        /// <summary>
+        /// Informs the listener of a change to the client's faction name.
+        /// </summary>
+        private void factionBox_TextChanged(object sender, EventArgs e)
+        {
+            if (factionBox.Text.Length > MAX_NAME_CHARS) listener.UpdateClientFactionName(factionBox.Text.Substring(0, MAX_NAME_CHARS));
+            else listener.UpdateClientFactionName(factionBox.Text);
+        }
+        /// <summary>
+        /// Informs the listener of a change to the client's player color,
+        /// if it's a valid color (otherwise does nothing).
+        /// </summary>
+        private void colorBox_TextChanged(object sender, EventArgs e)
+        {
+            string colorText = colorBox.Text;
+            if (colorText.Length != 7) return;
+            Color color;
+
+            try
+            {
+                color = ColorTranslator.FromHtml(colorText);
+            }
+            catch(Exception) // It throws an Exception, not any specific one
+            {
+                return; // Color can't be found
+            }
+
+            listener.UpdateClientColor(colorText);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            listener.StartGame();
+        }
+    }
+
+    /// <summary>
+    /// Defines a class's ability to respond to changes in client data
+    /// for a specific client, as well as its ability to respond to
+    /// the marking of a specific client as ready or not ready to 
+    /// begin the game.
+    /// The player change listener must have knowledge of which client
+    /// has been specified.
+    /// </summary>
+    public interface IPlayerChangeListener
+    {
+        void UpdateClientUsername(string newUsername);
+        void UpdateClientFactionName(string newFactionName);
+        void UpdateClientColor(string newColorHex);
+        void StartGame();
+    }
+}
