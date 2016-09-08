1f38d21a1239c3a328bad6ee507449fadc194d05
Robert Campbell <robertcampbellnz@gmail.com>
2016-09-08 14:52:20 +1200

    fix offset in movecommand

---
+++ Devious Retention SP/Entities/NullCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devious_Retention_SP
+{
+    public class NullCommand : Command
+    {
+        public void Execute()
+        {
+        }
+
+        public bool Tick()
+        {
+            return false;
+        }
+    }
+}
