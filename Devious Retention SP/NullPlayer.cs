0f603feea449f7cdc1e4f401d30185dce1df5e1a
R. Campbell <robertcampbellnz@gmail.com>
2016-11-16 13:45:48 +1300

    fix nullplayer to have a default color

--- Devious Retention SP/NullPlayer.cs
+++ Devious Retention SP/NullPlayer.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Devious_Retention_SP
 {
     public class NullPlayer : Player
     {
-        public NullPlayer(World world) : base(world)
+        public NullPlayer(World world) : base(world, Color.Black)
         {
         }
     }
